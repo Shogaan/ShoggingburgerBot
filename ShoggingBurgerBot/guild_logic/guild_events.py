a5a3846e514c8763afd4a25843082fa14912d490
Shogaan <andreika2002cool@gmail.com>
2020-04-22 16:58:40 +0300

    Fix error on join member when bot hasn't permissions

--- ShoggingBurgerBot/guild_logic/guild_events.py
+++ ShoggingBurgerBot/guild_logic/guild_events.py
@@ -10,18 +10,19 @@ class GuildEvents:
     async def on_bot_leave(self, guild):
         DatabaseProcessor()._remove_row_guild_settings(guild.id)
 
-    async def on_member_join(self, member):
+    async def on_member_join(self, member, bot):
         guild = member.guild
         channel = guild.system_channel if guild.system_channel is not None else guild.text_channels[0]
-        text = DatabaseProcessor()._get_greeting(guild.id).split("; ")
+        if channel.permissions_for(bot).send_messages:
+            text = DatabaseProcessor()._get_greeting(guild.id).split("; ")
 
-        emb = BASIC_EMB.copy()
-        emb.title = text[0].format(user=member.display_name, server=guild.name, prefix=PREFIX)
-        emb.description = text[1].format(user=member.mention, server=guild.name, prefix=PREFIX)
+            emb = BASIC_EMB.copy()
+            emb.title = text[0].format(user=member.display_name, server=guild.name, prefix=PREFIX)
+            emb.description = text[1].format(user=member.mention, server=guild.name, prefix=PREFIX)
 
-        if guild.id == 698939991891509258:
-            role = guild.get_role(698976288010010714)
-            await member.add_roles(role)
+            if guild.id == 698939991891509258:
+                role = guild.get_role(698976288010010714)
+                await member.add_roles(role)
 
-        await channel.send(embed=emb)
+            await channel.send(embed=emb)
 
